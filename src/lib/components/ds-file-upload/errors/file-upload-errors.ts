7f8d2a17ba04be39d9a79a4da2222e2579a88a08
aroba-dn <aroba@drivenets.com>
2025-11-17 13:33:26 +0200

    AR-31216: DAP | Design System | File Uploader - Implementation (review)

--- src/lib/components/ds-file-upload/errors/file-upload-errors.ts
+++ src/lib/components/ds-file-upload/errors/file-upload-errors.ts
@@ -7,10 +7,6 @@ export abstract class FileUploadError extends Error {
 	constructor(message: string) {
 		super(message);
 		this.name = this.constructor.name;
-		// Maintains proper stack trace for where our error was thrown (only available on V8)
-		if (Error.captureStackTrace) {
-			Error.captureStackTrace(this, this.constructor);
-		}
 	}
 }
 
