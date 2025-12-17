1a98a21887b83b31ec6321bcbf611410f7aa834e
Evyatar Daud <32631382+StyleShit@users.noreply.github.com>
2025-12-17 11:42:04 +0200

    build(design-system): move fonts loading to the vite plugin [AR-41333] (#7)

--- packages/vite-plugin-design-system/src/index.ts
+++ packages/vite-plugin-design-system/src/index.ts
@@ -5,6 +5,8 @@ export function vitePluginDesignSystem(): Plugin {
 		name: 'vite-plugin-design-system',
 		transformIndexHtml(html) {
 			const stylesheets = [
+				'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap',
+				'https://fonts.googleapis.com/css2?family=Fira+Mono:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap',
 				'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20,400,0..1,0&display=block',
 				'https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20,400,0..1,0&display=block',
 			];
