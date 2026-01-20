a4a66e525d3486507a756d9709b75cffee010289
Michal Murawski <michal.murawski@drivenets.com>
2026-01-20 10:48:40 +0100

    chore: change exports tests [AR-44783]

--- packages/design-system/tests/exports.test.ts
+++ packages/design-system/tests/exports.test.ts
@@ -2,20 +2,24 @@ import fs from 'node:fs';
 import { describe, expect, it } from 'vitest';
 
 describe('Design System exports', () => {
-	it('should export all components from index', () => {
-		// Arrange.
-		const expectedContent =
-			fs
-				.readdirSync('./src/components')
-				.toSorted()
-				.map((component) => {
-					return `export * from './components/${component}';`;
-				})
-				.join('\n') + '\n';
+	const expectedLines = fs
+		.readdirSync('./src/components')
+		.toSorted()
+		.map((component) => {
+			return `export * from './components/${component}';`;
+		});
 
-		// Assert.
-		const actualContent = fs.readFileSync('./src/index.ts', 'utf-8');
+	const actualContent = fs.readFileSync('./src/index.ts', 'utf-8');
+	const actualLines = actualContent.split('\n').filter((line) => line.trim().length > 0);
 
-		expect(actualContent).toBe(expectedContent);
+	expect(actualLines.length).toBe(expectedLines.length);
+
+	const tests = expectedLines.map((expectedLine, index) => {
+		return [actualLines[index], expectedLine];
+	});
+
+	// split expected by line and assert that each line is exported
+	it.each(tests)('"%s" is exported from the package', (actual, expected) => {
+		expect(actual).toBe(expected);
 	});
 });
